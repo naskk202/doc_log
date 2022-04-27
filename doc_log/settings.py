54aad62cfd107b64dc9111b4735816e58ecedf75
Atanas <atanas.as.ap@gmail.com>
2022-04-27 12:17:19 +0200

    Static root fiixed

--- doc_log/settings.py
+++ doc_log/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, 'static')
@@ -142,6 +142,14 @@ AUTH_USER_MODEL = "accounts.AppUserModel"
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 LOGGING = {
     'version': 1,
